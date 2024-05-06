3d3877c96babd4631b4343503ad50fc4691e1ae8
AmaroMOO <amaroromerocedrez@gmail.com>
2024-05-06 13:02:39 +0100

    tercera excepcion realizada

--- ejemplos_exceptions.py
+++ ejemplos_exceptions.py
@@ -7,16 +7,32 @@ class EjemploExcepciones:
         return num // den
     
     # ValueError
-
+    def zeroDivisionError(self, *, num:int, den:int)->int:
+        if (den == 0):
+            raise ZeroDivisionError
+        
+        return num // den
     # FileNotFoundError
+    def fileNotFoundError(self):
+        x = open()
+        if x != open:
+            raise FileNotFoundError("No existe")
+        
+        return x
+
 
     # TypeError
+    def typeError(self):
+        
+
+
+        raise TypeError
 
     # IndexError
 
-    # KeyboardInterrupt
+    # KeyboardInterrupt no
 
-    # UnicodeDecodeError
+    # UnicodeDecodeError no
 
     # AttributeError
 
