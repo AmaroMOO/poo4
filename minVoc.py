452c475af4874ff60195e2debb946240819e0b84
AmaroMOO <amaroromerocedrez@gmail.com>
2024-05-07 11:46:17 +0100

    creado archivo minvoc y comenzado

---
+++ minVoc.py
@@ -0,0 +1,9 @@
+import re
+
+class MinVoc:
+    def __init__(self) -> None:
+        pass
+    @staticmethod
+    def buscar(texto:str) -> None:
+        #patron palabras que empiezan en vocal minuscula
+        patron = ""
